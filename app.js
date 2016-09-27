ad88d7e27ed10bf6a050a3bb1b660ca7378d49b3
Robert Byrne <mr.rob.byrne@gmail.com>
2016-09-27 03:37:24 -0700

    Browserify Bootstrap ESLint React and more
    - Setup and installed many node modules.
    
    Setup of the app are complete.

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ var app = express();
 
 var port = 1337;
 
+app.get('/', function(req, res) {
+    res.send('Hello World');
+});
+
+app.get('/signup', function(req, res) {
+    res.send('Sign Up Page');
+});
+
 app.listen(port, function(err) {
     console.log('running server on port ' + port);
 });
---
+++ src/main.js
@@ -0,0 +1,9 @@
+/**
+ * Created by robert on 2016-09-27.
+ */
+
+$ = jQuery = require('jquery');
+
+var signUp = console.log('Sign Up Form, Coming Soon To A Screen Near You.');
+
+module.exports = App;
