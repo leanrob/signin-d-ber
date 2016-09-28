d9cef5045d1e45d675c1e72034145a7f38cfeb55
Robert Byrne <mr.rob.byrne@gmail.com>
2016-09-27 22:45:59 -0700

    Some finishing touches before merge
    Added a custom favicon
    made some small changes to index.html

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
 "use strict";
 
 var gulp = require('gulp');
-var connect = require('gulp-connect'); //Runs a local dev server
-var open = require('gulp-open'); //Open a URL in a web browser
+var connect = require('gulp-connect'); // Runs a local dev server
+var open = require('gulp-open'); // Open a URL in a web browser
 var browserify = require('browserify'); // Bundles JS
 var reactify = require('reactify');  // Transforms React JSX to JS
-var source = require('vinyl-source-stream'); // Use conventional text streams with Gulp
-var concat = require('gulp-concat'); //Concatenates files
-var lint = require('gulp-eslint'); //Lint JS files, including JSX
+var source = require('vinyl-source-stream'); // Text streams with Gulp
+var concat = require('gulp-concat'); // Concatenates files
+var lint = require('gulp-eslint'); // Lint JS files
 
 var config = {
 	port: 9005,
